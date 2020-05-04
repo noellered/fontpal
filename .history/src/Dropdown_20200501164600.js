dfd7258f706847a381cc22e8355afc96b3e9369a
Noelle Szombathy <business.noellered@gmail.com>
2020-05-03 17:53:32 -0700

    Added collapsible option groups for fonts

---
+++ .history/src/Dropdown_20200501164600.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Select from 'react-select';
+
+
+class Dropdown extends Component {
+
+    render(){
+
+        return(
+            <form className="textSelectors">
+              <label>Choose a {this.props.textType} Family:</label>
+              <Select value={this.props.value} onChange={this.props.onChange} options={this.props.options} styles={this.props.styles} menuIsOpen/>
+            </form>
+  
+        );
+    }
+}
+
+export default Dropdown;
