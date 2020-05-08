1d43678ca426fa59759f6550c27d956a7a857f3c
Noelle Szombathy <business.noellered@gmail.com>
2020-05-08 12:25:50 -0700

    Added Font Weight and changed layout

---
+++ .history/src/Body_20200507134628.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import WebFont from 'webfontloader';
+import TextareaAutosize from 'react-autosize-textarea';
+
+
+//Renders Header Text -- accepts fontFamily as prop
+class HeaderText extends Component {
+
+    state = {
+        userInput: "Change my font and text"
+    }
+
+    handleUserInput = (e) => {
+        this.setState({userInput: e.target.value});
+    } 
+
+    render(){
+        return(
+           <TextareaAutosize value={this.state.userInput} onChange={this.handleUserInput} key={this.props.fontFamily} spellcheck="false" className="header-text" style={{fontFamily: this.props.fontFamily, fontWeight: this.props.fontWeight}}></TextareaAutosize>
+        );
+    }
+}
+
+//Renders Span / Body Text -- accepts fontFamily as prop
+class SpanText extends Component {
+    render(){
+        return(
+            <TextareaAutosize spellcheck="false" className="body-text" style={{fontFamily: this.props.fontFamily, fontWeight: this.props.fontWeight}}>The text on this page is editable. You can try replacing it with your own to see what fonts will look like when paired. All fonts are available as Google WebFonts.</TextareaAutosize>
+        );
+    }
+}
+
+
+//Combines HeaderText and SpanText into one stylized div
+class Body extends Component {
+
+    state = {
+        headerValue: '',
+        bodyValue: '',
+        myGradient: 'blue',
+        headerLabel: '', //set labels to selected value
+        bodyLabel: '',
+        headerFontWeight: '700',
+        bodyFontWeight: '300'
+    }
+
+    //Sets font choice for Header text
+
+    handleHeaderChange = (e) => {
+        let fontChoice = e.value;
+        this.setState({headerValue: fontChoice, headerLabel: e});
+    }
+
+
+    //Sets font choice for Body text
+    handleBodyChange = (e) => {
+        let fontChoice = e.value;
+        this.setState({bodyValue: fontChoice, bodyLabel: e});
+    }
+    
+
+    //Sets gradient to selected 
+    handleGradientChange = selectedOption => {
+        this.setState({myGradient: selectedOption.value});
+      };
+
+    headerWeightChange = selectedOption => {
+        this.setState({headerFontWeight: selectedOption.value})
+    }
+
+    bodyWeightChange = selectedOption => {
+        this.setState({bodyFontWeight: selectedOption.value})
+    }
+
+    render(){
+
+        return(
+            <div> 
+                <Header onHeaderChange={this.handleHeaderChange} onHeaderWeightChange={this.headerWeightChange} onHeaderWeightChange={this.headerWeightChange}headerValue={this.state.headerValue} onBodyChange={this.handleBodyChange} bodyValue={this.state.bodyValue} onGradientChange={this.handleGradientChange}/>
+                <div className={`body-div ${this.state.myGradient}`}> 
+                    <HeaderText fontFamily={this.state.headerValue} fontWeight={this.state.headerFontWeight}/>
+                    <SpanText fontFamily={this.state.bodyValue} fontWeight={this.state.bodyFontWeight}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Body;
---
+++ .history/src/Dropdown_20200507171521.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Select from 'react-select';
+
+
+class Dropdown extends Component {
+
+    render(){
+
+        return(
+            <form className="textSelectors">
+              <label>{this.props.textType}</label>
+              <Select placeholder="Family" className="selector" value={this.props.value} onChange={this.props.onChange} options={this.props.options} styles={this.props.styles} menuIsOpen={this.props.open} formatGroupLabel={this.props.formatGroupLabel}/>
+            </form>
+  
+        );
+    }
+}
+
+export default Dropdown;
