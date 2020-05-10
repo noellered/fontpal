b2207c85dd27d3dd9810733b7ff88674a2b16443
Noelle Szombathy <business.noellered@gmail.com>
2020-05-09 22:55:32 -0700

    Updates

--- .history/src/FontPreview_20200427151246.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import Dropdown
-
-class FontPreview extends Componenet {
-    render(){
-        return();
-    }
-}
---
+++ precache-manifest.c49022374e4947d8e232203600d5c085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558471e7b817cf9eaad07f2fce5cc206",
+    "url": "/fontpal/index.html"
+  },
+  {
+    "revision": "bfb52a9eb68ff92d40ca",
+    "url": "/fontpal/static/css/main.50036123.chunk.css"
+  },
+  {
+    "revision": "e19cbb7cafd93a3e0cf5",
+    "url": "/fontpal/static/js/2.db85a199.chunk.js"
+  },
+  {
+    "revision": "bea58cb82d8ee5349c7906e65d936513",
+    "url": "/fontpal/static/js/2.db85a199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb52a9eb68ff92d40ca",
+    "url": "/fontpal/static/js/main.71c3605c.chunk.js"
+  },
+  {
+    "revision": "1bdd86745faab1f950cb",
+    "url": "/fontpal/static/js/runtime-main.1186118b.js"
+  }
+]);
